efd6b78e4e51398eba3a5170409d361254c63460
Kaushal-205 <20bce123@nirmauni.ac.in>
2024-04-01 11:09:45 +0530

    op-mainnet subgraph deployed

--- op-mainnet-subgraph/src/smart-disperse.ts
+++ op-mainnet-subgraph/src/smart-disperse.ts
@@ -1,45 +1,48 @@
 import {
   ERC20TokenDispersed as ERC20TokenDispersedEvent,
   EtherDispersed as EtherDispersedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent
-} from "../generated/SmartDisperse/SmartDisperse"
+  OwnershipTransferred as OwnershipTransferredEvent,
+} from "../generated/SmartDisperse/SmartDisperse";
 import {
   ERC20TokenDispersed,
   EtherDispersed,
-  OwnershipTransferred
-} from "../generated/schema"
-
+  OwnershipTransferred,
+} from "../generated/schema";
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 export function handleERC20TokenDispersed(
   event: ERC20TokenDispersedEvent
 ): void {
   let entity = new ERC20TokenDispersed(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._sender = event.params._sender
-  entity._token = event.params._token
-  entity._recipients = event.params._recipients
-  entity._values = event.params._values
+  );
+  entity._token = event.params._token;
+  // entity._recipients = event.params._recipients
+  entity._recipients = changetype<Bytes[]>(event.params._recipients);
+  entity._values = changetype<BigInt[]>(event.params._values);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // entity._values = event.params._values
 
-  entity.save()
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
 export function handleEtherDispersed(event: EtherDispersedEvent): void {
   let entity = new EtherDispersed(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._sender = event.params._sender
-  entity._recipients = event.params._recipients
-  entity._values = event.params._values
+  );
+  // entity._recipients = event.params._recipients;
+  // entity._values = event.params._values;
+  entity._recipients = changetype<Bytes[]>(event.params._recipients);
+  entity._values = changetype<BigInt[]>(event.params._values);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -47,13 +50,13 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
